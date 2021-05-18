426f00e0645f93f17f951def6e3319a46526b914
Firmansyah Sumardi <firmansyah@brilliance.com.my>
2021-05-18 10:29:14 +0800

    Add project files.

---
+++ FinanceMentor/Shared/EarningCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceMentor.Shared
+{
+    public enum EarningCategory
+    {
+        Salary,
+        CapitalGain,
+        Freelancing,
+        Coaching,
+        Flipping,
+        Gift,
+    }
+}
